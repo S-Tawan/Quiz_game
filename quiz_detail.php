3ffcf8273a046b96a3b4d21801d2ef3121670e33
MessageAlert <41282062+MessageAlert@users.noreply.github.com>
2018-12-08 14:33:35 +0700

    at quiz detail

--- quiz_detail.php
+++ quiz_detail.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>QuizSiJa</title>
     <?php
-        require 'link_title.php';
+    require 'link_title.php';
     ?>
 </head>
 <body style="background-color:#4d4d4d">
@@ -26,24 +26,24 @@
     <!-- check login -->
     <!-- Modal -->
     <div class="modal" tabindex="-1" role="dialog" id="createName">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
-        <p>Modal body text goes here.</p>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary">Save changes</button>
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-      </div>
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title">Modal title</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <p>Modal body text goes here.</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary">Save changes</button>
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+            </div>
+            </div>
+        </div>
     </div>
-  </div>
-</div>
     <script>$('#createName').modal('show')</script>
 </body>
 </html>
