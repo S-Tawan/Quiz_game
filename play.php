c41bef4a10a366638054d8e97c870925c1a6522c
MessageAlert <41282062+MessageAlert@users.noreply.github.com>
2018-12-08 18:15:17 +0700

    play me sija

---
+++ play.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <?php
+    require 'link_title.php';
+    ?>
+    <title>QuizSiJa</title>
+</head>
+<body style="background-color:#252525">
+    <div class="row">
+        <div class="col-lg-2" ></div>
+        <div class="col-lg-8">
+            <p style="margin:3%"></p>
+            <header style="background-color:red;">
+            <p>1</p>
+            </header>
+
+            <body style="background-color:green">
+            <p>2</p>
+            </body>
+
+            <footer style="background-color:blue">
+            <p>3</p>
+            </footer>
+
+        </div>
+        <div class="col-lg-2" ></div>
+    </div>
+</body>
+</html>
