d7adf0ebb35ce507f32d89afb12ddb5c5634ca91
S-Tawan <41279209+S-Tawan@users.noreply.github.com>
2018-12-09 20:36:39 +0700

    ry'

--- index.php
+++ index.php
@@ -58,14 +58,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>QuizSiJa</title>
-
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-
+    <?php
+    require 'link_title.php';
+    ?>
 </head>
-<body >
+<body style="background-color:#252525">
     <!-- navbar sija -->
     <nav class="navbar navbar-inverse">
         <div class="container-fluid">
@@ -180,8 +177,9 @@
             </div>
             <div class="col-sm-4"></div>
         </div>
+    
+        
+    
 
 </body>
-
-
 </html>
