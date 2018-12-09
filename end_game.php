72dc543d4dc9fd79dfee96d1ca0f88e84cfad4b0
MessageAlert <41282062+MessageAlert@users.noreply.github.com>
2018-12-09 20:50:40 +0700

    end day 2

--- end_game.php
+++ end_game.php
@@ -1,8 +1,8 @@
 <?php
-    // require 'server.php';
-    // echo $_SESSION['score'].'<br>';
-    // echo $_SESSION['correct'].'/'.count($_SESSION['question']);
-    // echo "<br>". $_SESSION['quiz'];
+    require 'server.php';
+    echo $_SESSION['score'].'<br>';
+    echo $_SESSION['correct'].'/'.count($_SESSION['question']);
+    echo "<br>". $_SESSION['quiz'];
 ?>
 <!DOCTYPE html>
 <html lang="en">
